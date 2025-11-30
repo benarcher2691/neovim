bb3b97e0284c38ae1de91605c8d8ae9d696e2bb4
Ben Archer <ben.archer2691@gmail.com>
2025-11-30 09:43:15 +0100

    fix(api): allow reading 'term' option via nvim_get_option_value
    The 'term' option is a special option not in the regular option table.
    It was inaccessible via the API (and thus vim.o.term) since the refactor
    in #25716.
    
    This adds read-only support for 'term' in nvim_get_option_value() by:
    - Not erroring on 'term' in validate_option_value_args()
    - Calling get_tty_option() when opt_idx is invalid
    - Explicitly rejecting SET operations for 'term' (read-only)
    
    Note: 'ttytype' and 't_Co' are not exposed as they are deprecated.
    
    Fixes #31860

--- test/functional/api/vim_spec.lua
+++ test/functional/api/vim_spec.lua
@@ -1977,6 +1977,13 @@ describe('API', function()
       ]])
       eq(false, api.nvim_get_option_value('modified', {}))
     end)
+
+    it('tty options #31860', function()
+      -- 'term' is a special option not in the regular option table (read-only)
+      eq('string', type(api.nvim_get_option_value('term', {})))
+      -- Setting 'term' should fail
+      eq("Unknown option 'term'", pcall_err(api.nvim_set_option_value, 'term', 'foo', {}))
+    end)
   end)
 
   describe('nvim_{get,set}_current_buf, nvim_list_bufs', function()
